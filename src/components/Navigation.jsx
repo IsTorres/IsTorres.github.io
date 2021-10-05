dbe108ae76ca71ed3447f2c681b00333d6ee32a5
isTorres <ismaeltorres612@gmail.com>
2021-10-05 14:14:41 -0300

    create navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div className='card'>
+        <h3 className='title'>Navigation</h3>
+        <h4><a className='text-link' href="https://www.betrybe.com/" target="_blank" rel="noreferrer">Link de projeto</a></h4>
+        <h4><a className='text-link' href="https://www.betrybe.com/" target="_blank" rel="noreferrer">Link de projeto</a></h4>
+        <h4><a className='text-link' href="https://www.betrybe.com/" target="_blank" rel="noreferrer">Link de projeto</a></h4>
+        <h4><a className='text-link' href="https://www.betrybe.com/" target="_blank" rel="noreferrer">Link de projeto</a></h4>
+      </div>
+    )
+  }
+}
