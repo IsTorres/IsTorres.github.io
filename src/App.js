b9ff02bd2c8318326b19cf765e868eed4cb397d2
isTorres <ismaeltorres612@gmail.com>
2021-10-05 13:51:05 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1 style={ { color: 'black' } } >dale</h1>
       </header>
     </div>
   );
