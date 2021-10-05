8bac7e8ac44db57753759d2464fa3faaad8bad8a
isTorres <ismaeltorres612@gmail.com>
2021-10-05 14:13:20 -0300

    create bg

---
+++ src/components/Background.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import Particles from "react-tsparticles";
+
+export default function Background() {
+  // particlesInit(main) {
+  //   console.log(main);
+
+  //   // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // }
+
+  // particlesLoaded(container) {
+  //   console.log(container);
+  // }
+
+  return (
+    <Particles
+      id="tsparticles"
+      // init={this.particlesInit}
+      // loaded={this.particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#0e1522",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 20,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#747983",
+          },
+          links: {
+            color: "#747983",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 2,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 1000,
+            },
+            value: 50,
+          },
+          opacity: {
+            value: 0.3,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+}
