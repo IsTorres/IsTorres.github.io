9b309c523998866ff485bb945334666add3abfd2
isTorres <ismaeltorres612@gmail.com>
2022-02-08 17:35:38 -0300

    componetiza... portfolio new design

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './styles/App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
+import TabPanel from './components/TabPanel'
+// import { BrowserRouter as Router, Link } from "react-router-dom";
+// import Container from 'react-bootstrap/Container';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Navbar';
+// import Bio from './components/Bio';
+// import Contact from './components/Contact';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: 'Ismael Torres',
-      links: [
-        { title: 'Home', path: '/' },
-        { title: 'About', path: '/about' },
-        { title: 'Projects', path: '/projects' },
-        { title: 'Contact', path: '/contact' },
-      ],
-      home: {
-        title: 'Ismael Torres',
-        subtitle: 'Web Developer',
-        text: 'Checkout my projects',
-      },
-      about: {
-        title: 'About me',
-      },
-      projects: {
-        title: 'My projects',
-      },
-      contact: {
-        title: 'Lets talk',
-      },
-    };
-  }
-
-  render() {
-    return (
-      <Router>
-        <Container>
-          <p>Hello</p>
-          {/* <BrowserRouter>
-            <Routes>
-              <Route path='/'>Home</Route>
-              <Route path='/about' element={Bio}>About</Route>
-              <Route path='/projects'>Projetos</Route>
-              <Route path='/contact' element={Contact}>Contato</Route>
-            </Routes>
-          </BrowserRouter> */}
-        </Container>
-      </Router>
-    );
-  }
+export default function App() {
+  return <div>
+    <TabPanel />
+  </div>;
 }
