57c20b1681ebc05ace1aa5123594710f8c15b6df
isTorres <ismaeltorres612@gmail.com>
2022-02-08 11:49:27 -0300

    rebild portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import './styles/App.css';
-import Bio from './components/Bio';
-import Contact from './components/Contact';
-import Navigation from './components/Navigation';
-import Background from './components/Background';
+// import Bio from './components/Bio';
+// import Contact from './components/Contact';
+// import Navigation from './components/Navigation';
+// import Background from './components/Background';
 
-function App() {
-  return (
-    <div className="App">
-      <div className='content'>
-        <section className='App-header'>
-          <Bio />
-          <Contact />
-        </section>
-        <section className='App-header'>
-          <Navigation />
-        </section>
-      </div>
-      <Background />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className='content'>
+//         <section className='App-header'>
+//           <Bio />
+//           <Contact />
+//         </section>
+//         <section className='App-header'>
+//           <Navigation />
+//         </section>
+//       </div>
+//       <Background />
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: 'Ismael Torres',
+      links: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Projects', path: '/projects' },
+        { title: 'Contact', path: '/contact' },
+      ],
+      home: {
+        title: 'Ismael Torres',
+        subtitle: 'Web Developer',
+        text: 'Checkout my projects',
+      },
+      about: {
+        title: 'About me',
+      },
+      projects: {
+        title: 'My projects',
+      },
+      contact: {
+        title: 'Lets talk',
+      },
+    }
+  }
+  render() {
+    return <div></div>;
+  }
 }
 
-export default App;
